79855948dc9d2f85f1972469ceda4d2f646c3ae0
ankit <ankitraj.21.91@gmail.com>
2022-01-27 17:25:25 +0530

    changed name

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Ankit')
+    print_hi('Ayush')
     print_hi('How are you?????')
     print_hi ("What is the time")
 
